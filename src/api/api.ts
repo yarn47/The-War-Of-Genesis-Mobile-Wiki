53e64e4a3a3642688de72fca71f28a1584359a48
yarn47 <47959252+yarn47@users.noreply.github.com>
2026-06-05 18:51:43 +0900

    보안 수정

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 const api = axios.create({
     // 환경 변수가 있으면 그걸 쓰고, 없으면 로컬 호스트를 바라보게 세팅!
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
